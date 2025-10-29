3d3152d6855eb3cab3baf8972d1bc169e56a0855
HanChanghoon <hanpro93@naver.com>
2025-10-29 21:48:08 +0900

    [한창훈][Class2] LockFreeStack 실습

--- GameServer/GameServer/ConcurrentStack.h
+++ GameServer/GameServer/ConcurrentStack.h
@@ -1,157 +1,89 @@
 #pragma once
 
 #include <mutex>
-
-template<typename T>
-class LockStack
-{
-public:
-	LockStack() {}
-
-	LockStack(const LockStack&) = delete;
-	LockStack& operator=(const LockStack&) = delete;
-
-	void Push(const T value)
-	{
-		lock_guard<mutex> lock(_mutex);
-		_stack.push(std::move(value));
-		_CV.notify_one();
-	}
-
-	bool TryPop(T& value)
-	{
-		lock_guard<mutex> lock(_mutex);
-		if (_stack.empty())
-			return false;
-
-		value = std::move(_stack.top());
-		_stack.pop();
-		return true;
-	}
-
-	void WaitPop(T& value)
-	{
-		unique_lock<mutex> lock(_mutex);
-		_CV.wait(lock, [this] {return false == _stack.empty(); });
-		value = std::move(_stack.top());
-		_stack.pop();
-	}
-
-private:
-	stack<T>			_stack;
-	mutex				_mutex;
-	condition_variable	_CV;
-};
+#include <atomic>
 
 template<typename T>
 class LockFreeStack
 {
+	struct Node;
+
+	struct CountedNodePtr
+	{
+		int32 externalCount = 0;
+		Node* ptr = nullptr;
+	};
+
 	struct Node
 	{
-		Node(const T& value) : _data(value)
+		Node(const T& value) : _data(make_shared<T>(value))
 		{
-			
+
 		}
 
-		T		_data;
-		Node*	_next = nullptr;
+		shared_ptr<T>	_data;
+		CountedNodePtr	_next;
+		atomic<int32>	_internalCount = 0;
 	};
 
 public:
 	void Push(const T& value)
 	{
-		Node* node	= new Node(value);
-		node->_next = _head;
+		CountedNodePtr node;
+		node.ptr = new Node(value);
+		node.externalCount = 1;
 
-		while (false == _head.compare_exchange_weak(node->_next, node))
+		node.ptr->_next = _head;
+		while (false == _head.compare_exchange_weak(node.ptr->_next, node))
 		{
 
 		}
 	}
 
-	bool TryPop(T& value)
+	shared_ptr<T> TryPop()
 	{
-		++_popCount;
-
-		Node* oldHead = _head;
-
-		while ((nullptr != oldHead) && (false == _head.compare_exchange_weak(oldHead, oldHead->_next)))
+		CountedNodePtr oldHead = _head;
+		while (true)
 		{
+			IncreaseHeadCount(oldHead);
+			Node* ptr = oldHead.ptr;
 
+			if (nullptr == ptr)
+				return shared_ptr<T>();
+
+			if (true == _head.compare_exchange_strong(oldHead, ptr->_next))
+			{
+				shared_ptr<T> result;
+				result.swap(ptr->_data);
+
+				const int32 countIncrease = oldHead.externalCount - 2;
+
+				if (ptr->_internalCount.fetch_add(countIncrease) == -countIncrease)
+					delete ptr;
+
+				return result;
+			}
+			else if (ptr->_internalCount.fetch_sub(1) == 1)
+			{
+				delete ptr;
+			}
 		}
+	}
 
-		if (nullptr == oldHead)
+	void IncreaseHeadCount(CountedNodePtr& oldCounter)
+	{
+		while (true)
 		{
-			--_popCount;
-			return false;
+			CountedNodePtr newCounter = oldCounter;
+			++newCounter.externalCount;
+			if (_head.compare_exchange_strong(oldCounter, newCounter))
+			{
+				oldCounter.externalCount = newCounter.externalCount;
+				break;
+			}
 		}
-
-		value = oldHead->_data;
-		TryDelete(oldHead);
-		
-		return true;
 	}
 	
 private:
-	void TryDelete(Node* oldHead)
-	{
-		if (1 == _popCount)
-		{
-			Node* node = _pendingList.exchange(nullptr);
-
-			if (--_popCount == 0)
-			{
-				DeleteNodes(node);
-			}
-			else if(nullptr != node)
-			{
-				ChainPendingNodeList(node);
-			}
-
-			delete oldHead;
-		}
-		else
-		{
-			ChainPendingNode(oldHead);
-			--_popCount;
-		}
-	}
-
-	void ChainPendingNodeList(Node* first, Node* last)
-	{
-		last->_next = _pendingList;
-		
-		while (false == _pendingList.compare_exchange_weak(last->_next, first))
-		{
-
-		}
-	}
-
-	void ChainPendingNodeList(Node* node)
-	{
-		Node* last = node;
-		while (nullptr != last->_next)
-			last = last->_next;
-
-		ChainPendingNodeList(node, last);
-	}
-
-	void ChainPendingNode(Node* node)
-	{
-		ChainPendingNodeList(node, node);
-	}
-
-	static void DeleteNodes(Node* node)
-	{
-		while (nullptr != node)
-		{
-			Node* next = node->_next;
-			delete node;
-			node = next;
-		}
-	}
-
-	atomic<Node*>	_head			= nullptr;
-	atomic<Node*>	_pendingList	= nullptr;
-	atomic<uint32>	_popCount		= 0;
+	atomic<CountedNodePtr> _head;
 };
--- GameServer/GameServer/GameServer.cpp
+++ GameServer/GameServer/GameServer.cpp
@@ -26,9 +26,9 @@ void Pop()
 {
 	while (true)
 	{
-		int32 data = 0;
-		if (true == s.TryPop(OUT data))
-			cout << data << endl;
+		auto data = s.TryPop();
+		if (nullptr != data)
+			cout << (*data) << endl;
 	}
 }
 
